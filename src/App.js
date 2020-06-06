e630a125ae2fc820d708c6a2979b669a5504919e
ebaek <esther.baek@ibm.com>
2020-06-06 15:46:56 -0400

    make v1 site

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import Navbar from './components/navbar.jsx';
+import About from './components/about.jsx';
+import MoreAbout from './components/moreAbout.jsx';
+import Projects from './components/projects.jsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <Navbar />
+
+          <Switch>
+            <Route path="/about">
+              <MoreAbout />
+            </Route>
+
+            <Route path="/projects">
+              <Projects />
+            </Route>
+
+            <Route path="/">
+              <About />
+            </Route>
+            
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/components/navbar.jsx
@@ -0,0 +1,34 @@
+import React, { history } from 'react';
+import Popup from './popup';
+import { Link } from 'react-router-dom';
+import '../styling/navbar.css';
+
+const Navbar = () => {
+    const [listOpen, setListOpen] = React.useState(false);
+
+    return(
+        <div>
+            {listOpen &&
+                <Popup setListOpen={setListOpen} listOpen={listOpen}/>
+            }
+            <div className="navbar-wrapper">
+                <Link className="title" to="/">ESTHER BAEK</Link>
+                <button className="nav-list-button" onClick={() => setListOpen(!listOpen)}>
+                    {!listOpen && 
+                    <svg className="bi bi-list" width="2rem" height="2rem" viewBox="0 0 16 16" fill="slategray" xmlns="http://www.w3.org/2000/svg">
+                        <path fillRule="evenodd" d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                    </svg>
+                    }
+
+                    {listOpen && <svg className="bi bi-x" width="2rem" height="2rem" viewBox="0 0 16 16" fill="slategray" xmlns="http://www.w3.org/2000/svg">
+                        <path fillRule="evenodd" d="M11.854 4.146a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708-.708l7-7a.5.5 0 0 1 .708 0z"/>
+                        <path fillRule="evenodd" d="M4.146 4.146a.5.5 0 0 0 0 .708l7 7a.5.5 0 0 0 .708-.708l-7-7a.5.5 0 0 0-.708 0z"/>
+                    </svg>}
+                </button>
+            </div>
+        </div>
+        
+    );
+}
+
+export default Navbar;
