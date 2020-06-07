dffd12989b704ccd2f07033cd58d71275ea2b64e
ebaek <esther.baek@ibm.com>
2020-06-06 22:38:37 -0400

    Revert "Add favicon"
    This reverts commit e0ba0307cd3999be1921bdf38cb7c9cb2eb2c115, reversing
    changes made to e009fc9686f4fff785b50ada77e131b396c47145.

---
+++ src/components/about.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Terminal from "./terminal"
+
+import "../styling/about.css"
+
+const About = () => {
+    return(
+        <div className="about-wrapper">
+            <Terminal/>
+        </div>
+    )
+}
+
+export default About
