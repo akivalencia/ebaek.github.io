0d411a40cf99925ec8d982f77cbcdde05f444670
Esther Baek <estherbaek@google.com>
2021-01-02 13:32:07 -0500

    Change back to react

---
+++ precache-manifest.ccdee6850c843be00f520ef33e9d00b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "403318e8fe748a9e10855684f971df72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7e7af070b440c7411d5",
+    "url": "/static/css/main.dc553f70.chunk.css"
+  },
+  {
+    "revision": "71bf8e51552fb740cae0",
+    "url": "/static/js/2.a4098ee5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a4098ee5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7e7af070b440c7411d5",
+    "url": "/static/js/main.d86d35c6.chunk.js"
+  },
+  {
+    "revision": "2792796446ac0b17369c",
+    "url": "/static/js/runtime-main.3f4b3659.js"
+  }
+]);
